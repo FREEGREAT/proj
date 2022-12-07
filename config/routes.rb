4707f416cf396b40528d5ade2b279bace0d4db37
FREEGREAT <vladdmitrenko3009@gmail.com>
2022-12-07 10:20:11 +0200

    create item table && style it

--- config/routes.rb
+++ config/routes.rb
@@ -1,11 +1,11 @@
 Rails.application.routes.draw do
+  resources :items
   devise_for :users
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
   # Defines the root path route ("/")
   root "welcome#index"
 
-  get 'about', to: 'welcome#about'
-  get 'table', to: 'managers#table'
+  get 'table', to: 'items#index'
   get 'graphics', to: 'managers#graphics'
 end
